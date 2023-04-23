efd4db0417e049c73a5f4510804c57d3d13e971d
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-04-23 12:29:32 +0430

    navbar compomemt created

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './index.css'
-
-
-export default function  App() {
-  return (
-    <div className="text-center bg-black text-white font-bold">Hello World!</div>
-  )
-}
---
+++ src/home/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '/src/index.css'
+import Navbar from '../global_components/navbar/navbar'
+
+
+export default function  App() {
+  return (
+    <Navbar />
+  )
+}
