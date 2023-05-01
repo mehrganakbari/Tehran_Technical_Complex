c1725c01ecb9992f107f272b8d34707b5c6bbfb4
mehrgan akbari <akbarimehrgan@yahoo.com>
2023-05-01 12:34:11 +0430

    push on github

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import AboutUs from "./pages/about_us/about_us";
 import ContactUs from "./pages/contact_us/contact_us";
@@ -11,13 +11,27 @@ import Navbar from "./layout/navbar/navbar";
 // => ContactUs => "./pages/contact_us/contact_us"
 // => Shop => "./pages/shop/shop"
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about-us",
+      element: <AboutUs />,
+    },
+    {
+      path: "/contact-us",
+      element: <ContactUs />,
+    },
+    {
+      path: "/shop",
+      element: <Shop />,
+    },
+  ]);
   return (
-    <BrowserRouter>
+    <RouterProvider router={router} >
       <Navbar />
-      <Route path="/" Component={Home} />
-      <Route path="/about-us" Component={AboutUs} />
-      <Route path="/contact-us" Component={ContactUs} />
-      <Route path="/shop" Component={Shop} />
-    </BrowserRouter>
+    </RouterProvider>
   );
 }
